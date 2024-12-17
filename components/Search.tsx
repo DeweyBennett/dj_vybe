3f3058eebf789ce088d82486c4142dd84cc9df0e
DeweyBennett <debennett3@gmail.com>
2024-12-17 10:57:56 -0500

    v0.0.1

--- components/Search.tsx
+++ components/Search.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react'
 import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { Suspense } from 'react'
 
 const Search = ({ placeholder = 'Search title...' }: { placeholder?: string }) => {
   const [query, setQuery] = useState('');
@@ -34,12 +35,14 @@ const Search = ({ placeholder = 'Search title...' }: { placeholder?: string }) =
 
   return (
     <div className="flex items-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
-      <input 
-        type="text"
-        placeholder={placeholder}
-        onChange={(e) => setQuery(e.target.value)}
-        className="text-[16px] font-normal leading-[24px] border bg-white border-primary outline-offset-0 placeholder:text-grey-500 focus:border-2 focus:border-secondary focus-visible:ring-0  w-full rounded-md  border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none  focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-      />
+      <Suspense>
+        <input 
+          type="text"
+          placeholder={placeholder}
+          onChange={(e) => setQuery(e.target.value)}
+          className="text-[16px] font-normal leading-[24px] border bg-white border-primary outline-offset-0 placeholder:text-grey-500 focus:border-2 focus:border-secondary focus-visible:ring-0  w-full rounded-md  border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none  focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+        />
+      </Suspense>
     </div>
   )
 }
